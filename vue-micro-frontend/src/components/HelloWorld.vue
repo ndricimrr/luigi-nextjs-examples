604aa4078f14fdb39e3efde0d478b613648aedf2
Ndricim Rrapi <ndricim.rrapi@sap.com>
2022-04-19 20:02:33 +0200

    Add example 2

---
+++ vue-micro-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h1>vue-microfrotend is running</h1>
+   <p style="color: red" >Current Microfrontend URL: <b>{{url}}</b></p>
+</div>
+
+</template>
+
+<script>
+export default {
+  data: function() {
+        return {
+            url: window.location.href
+        }
+    },
+}
+</script>
